d5d0d9c19a962b9be490392c4878849c00afa017
Deepak <deepak.upreti@icloud.com>
2020-04-12 00:33:53 +0530

    comments added

--- app/src/main/java/com/upreti/alertdialog/MainActivity.java
+++ app/src/main/java/com/upreti/alertdialog/MainActivity.java
@@ -10,14 +10,21 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Declare variables
 Button vButton;
-AlertDialog.Builder alb;
+AlertDialog.Builder alb; //alb is a variable for AlertDialog.builder class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        alb = new AlertDialog.Builder(MainActivity.this);
+
+        //Initialise variable and connect the xml to Java code
         vButton = findViewById(R.id.button);
+
+        //Create a new object for accessing AlertDialog.builder class
+        alb = new AlertDialog.Builder(MainActivity.this);
+
+        //Access the methods of AlertDialog.builder class using the object "alb"
         alb.setCancelable(true);
         alb.setTitle("Warning!");
         alb.setMessage("Do you really want to delete this?");
@@ -26,7 +33,9 @@ AlertDialog.Builder alb;
         alb.setNegativeButton("No",null);
 
     }
-
+    // Method for button listener.
+    // Call the show method of AlertDialog.builder class using the object "alb"
+    //Toast is just for verification
     public void alert(View view) {
         alb.show();
         Toast.makeText(this, "You clicked button!", Toast.LENGTH_SHORT).show();
